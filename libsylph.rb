bf17534b89efb4df4c152c70aa189479546e82d7
Frank Erens <frank@synthi.net>
2012-10-21 18:11:31 +0200

    Now LibSylph no longer needs --HEAD flag.

--- libsylph.rb
+++ libsylph.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Libsylph < Formula
   homepage 'http://libsylph.sourceforge.net'
+  url 'https://github.com/SeySayux/libsylph.git'
   head 'https://github.com/SeySayux/libsylph.git'
   version '0.2'
 
