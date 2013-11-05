83d06017853b8ff1d8a5aa84129b2fdbc2773e3d
Frank Erens <frank@synthi.net>
2013-11-05 14:50:02 +0100

    Two more binutils.

---
+++ arm-eabi-binutils.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class ArmEabiBinutils < Formula
+  homepage 'http://www.gnu.org/software/binutils/binutils.html'
+  url 'http://ftpmirror.gnu.org/binutils/binutils-2.23.2.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.2.tar.gz'
+  sha1 'c3fb8bab921678b3e40a14e648c89d24b1d6efec'
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--infodir=#{info}",
+                          "--mandir=#{man}",
+                          "--disable-werror",
+                          "--enable-interwork",
+                          "--disable-multilib",
+                          "--disable-nls",
+                          "--target=arm-eabi"
+    system "make"
+    system "make install"
+  end
+end
